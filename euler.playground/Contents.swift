e6a46bba0042eb41390b96176f0ebf0e054a0326
Alex Aylesbury <alex@matica.com>
2018-12-30 20:46:23 +0000

    Force

--- euler.playground/Contents.swift
+++ euler.playground/Contents.swift
@@ -1,6 +1,6 @@
 import UIKit
 
-func problem1BruteForce()
+func problem_1BruteForce()
 {
     var sum = 0
     for n in Range(3...999) {
@@ -8,7 +8,40 @@ func problem1BruteForce()
             sum += n
         }
     }
-    print("\(sum)")
+    print("problem 1 : \(sum)")
 }
 
-problem1BruteForce()
+/*
+summation = (n/2)(a+l)
+n - number of terms, a - first term, l - last term
+*/
+func problem_1()
+{
+    // multiples of 3 up to 990
+    let m3 = 165 * 993
+    // multiples of 5 up to 990
+    let m5 = 99 * 995
+    // counted twice multiples of 15
+    let m15 = 33 * 1005
+    let answer = m3 + m5 - m15 + 993 + 995 + 996 + 999
+    print("problem 1 : \(answer)")
+}
+
+func problem_2BruteForce()
+{
+    var previousFib = 1
+    var fib = 2
+    var sum = 0
+    while fib < 4000000 {
+        if fib % 2 == 0 {
+            sum += fib
+        }
+        let newFib = previousFib + fib
+        previousFib = fib
+        fib = newFib
+    }
+    print("problem 2 : \(sum)")
+}
+problem_1BruteForce()
+problem_1()
+problem_2BruteForce()
